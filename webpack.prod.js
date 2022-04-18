64c3ca028e4e97a8a4f7830b3e6a75e2404d6857
Belov Roman <rnbelov96@gmail.com>
2022-04-18 06:56:38 +0300

    Setup eslint

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,74 +6,79 @@ const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-[hash].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[hash].js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+              publicPath: './dist/styles',
+              reloadAll: true,
             },
-            {
-                test: /\.(sass|scss|css)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                            publicPath: './dist/styles',
-                            reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|webp)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                    },
-                }, 'image-webpack-loader'],
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true,
-                    }
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: require('html-webpack-template'),
-            title: 'TODO App',
-            mobile: true,
-            lang: 'en-US',
-            appMountId: 'root'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]-[hash].css'
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new OptimizeCssPlugin(),
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-            }),
+          },
+          'css-loader',
+          'sass-loader',
         ],
-    }
-}
+      },
+      {
+        test: /\.(png|jpg|gif|webp)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+          'image-webpack-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      title: 'TODO App',
+      mobile: true,
+      lang: 'en-US',
+      appMountId: 'root',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[hash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssPlugin(),
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+      }),
+    ],
+  },
+};
