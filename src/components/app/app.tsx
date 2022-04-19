1a498d97e577e76e538ba171715c02cb13f4ab56
Belov Roman <rnbelov96@gmail.com>
2022-04-19 12:08:11 +0300

    Install and setup packages for testing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ControlPanel } from 'components/control-panel';
-import { CreateForm } from '@/components/create-form';
+import { CreateForm } from 'components/create-form';
 import { TODOList } from 'components/TODO-list';
 
-export const App = () => (
+export default () => (
   <div className="container">
     <h1 className="title">todos</h1>
     <div className="content">
